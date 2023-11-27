2107a6da7574f6180b8b2ee50fe5f451d2402b89
Emilse Arias <emilsearias@gmail.com>
2023-11-27 11:24:17 -0300

    practicamos con los tipos de export

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import {Contacto, prueba} from "./classContacto.js";
+
+const contactoNuevo = new Contacto(1,'algun nombre', 'apellido', 'sadfsdf@fsdf.com', 23423423);
+console.log(contactoNuevo)
+
+console.log(prueba);
---
+++ js/classContacto.js
@@ -0,0 +1,54 @@
+ export class Contacto {
+    #id;
+    #nombre;
+    #apellido;
+    #email;
+    #telefono;
+  
+    constructor(id , nombre, apellido, email, telefono) {
+      this.#id = id;
+      this.#nombre = nombre;
+      this.#apellido = apellido;
+      this.#email = email;
+      this.#telefono = telefono;
+    }
+  
+    get id() {
+      return this.#id;
+    }
+  
+    get nombre() {
+      return this.#nombre;
+    }
+  
+    set nombre(nuevoNombre) {
+      this.#nombre = nuevoNombre;
+    }
+  
+    get apellido() {
+      return this.#apellido;
+    }
+  
+    set apellido(nuevoApellido) {
+      this.#apellido = nuevoApellido;
+    }
+  
+    get email() {
+      return this.#email;
+    }
+  
+    set email(nuevoEmail) {
+      this.#email = nuevoEmail;
+    }
+  
+    get telefono() {
+      return this.#telefono;
+    }
+  
+    set telefono(nuevoTelefono) {
+      this.#telefono = nuevoTelefono;
+    }
+  
+  }
+  
+export const prueba = 234234
