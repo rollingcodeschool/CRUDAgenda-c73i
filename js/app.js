fce5eec2b46977a164a94f11969cb75c0f122bfb
Emilse Arias <emilsearias@gmail.com>
2023-11-29 10:24:49 -0300

    creamos un objeto que guardamos en la agenda y esta es almacenada en localstorage

--- js/app.js
+++ js/app.js
@@ -3,14 +3,36 @@ import Contacto from "./classContacto.js";
 //variables globales
 // const contactoNuevo = new Contacto(1,'algun nombre', 'apellido', 'sadfsdf@fsdf.com', 23423423);
 const agenda = [];
-const formularioContacto = document.querySelector('form');
+const formularioContacto = document.querySelector("form");
+const nombre = document.querySelector("#nombre"),
+  apellido = document.querySelector("#apellido"),
+  email = document.querySelector("#email"),
+  telefono = document.querySelector("#telefono");
 console.log(formularioContacto);
 
 // funciones
-const crearContacto = ()=>{
+const crearContacto = (e) => {
+  e.preventDefault();
+  console.log("desde la funcion crear Contacto");
+  //validar los datos ingresados por el usuario
+  //crear el objeto con los datos del formulario
+  const contactoNuevo = new Contacto(
+    1,
+    nombre.value,
+    apellido.value,
+    email.value,
+    telefono.value
+  );
+  //guardar el objeto en un array agenda
+  agenda.push(contactoNuevo);
+  //guardar la agenda en localstorage
+  guardarEnLocalstorage();
+  console.log(agenda);
+};
 
+const guardarEnLocalstorage = () =>{
+    localStorage.setItem('agendaKey', JSON.stringify(agenda));
 }
 
 // logica
-formularioContacto.addEventListener('submit', crearContacto)
-
+formularioContacto.addEventListener("submit", crearContacto);
--- js/classContacto.js
+++ js/classContacto.js
@@ -48,6 +48,16 @@
     set telefono(nuevoTelefono) {
       this.#telefono = nuevoTelefono;
     }
+
+    toJSON(){
+      return {
+        id: this.id,
+        nombre: this.nombre,
+        apellido: this.apellido,
+        email: this.email,
+        telefono: this.telefono
+      }
+    }
   
   }
   
